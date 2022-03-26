912863932451d5c6469953e985d11f531fbc6305
Elimane Fofana <elimane.fofana@supinfo.com>
2022-03-26 06:38:09 +0100

    new

--- Vote App/vote_app/src/pages/voter/electeurs.js
+++ Vote App/vote_app/src/pages/voter/electeurs.js
@@ -65,24 +65,17 @@ const Electeurs = (props) => {
     console.log(allCandidates);
     return Array.from(allCandidates).map((item, index)=>{
       let imgUrl = `https://bootdey.com/img/Content/avatar/avatar${item.id}.png`;
-      return <div key={index} className="card text-center mt-3" style={{width: '15rem',marginLeft: 12,backgroundColor: '#FBFBFB'}}>
-        <img src={imgUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{item.name}</h5>
-            <p className="card-text">Candidat à l'élection</p>
-          </div>
-          <h5><span className="badge badge bg-success">{item.voteCount}</span></h5>
-          <div className="card-body">
-            <div className="d-flex flex-row bd-highlight mb-1">
-              <div className="p-2 bd-highlight">
-                <button disabled={isEnded == 'true'} href="#" className="btn btn-outline-primary">Voter</button>
-              </div>
-              <div className="p-2 bd-highlight">
-                <button disabled={isEnded == 'true'} href="#" className="btn btn-outline-secondary">Déléguer</button>
-              </div>
-            </div>
-          </div>
-      </div>
+      return <Card key={index} className="text-center mt-3" style={{width: '15rem',marginLeft: 12,backgroundColor: '#FBFBFB'}}>
+              <Card.Img variant="top" src={imgUrl} />
+              <Card.Body>
+                <Card.Title>{web3.utils.hexToUtf8(item.name)}</Card.Title>
+                <Card.Text><div className=""><h5><Badge bg="success">{item.voteCount}</Badge></h5></div></Card.Text>
+                <Stack direction="horizontal" gap={3}>
+                  <div><Button disabled={isEnded == 'true'} variant="primary">Voter</Button></div>
+                  <div><Button disabled={isEnded == 'true'} variant="secondary">Déléguer</Button></div>
+                </Stack>
+              </Card.Body>
+            </Card>
     });
   }
 
@@ -112,7 +105,47 @@ const Electeurs = (props) => {
 
     return (
       <Container>
-
+        <Row className="mt-5">
+          <Col> <h3>Espace électeurs</h3></Col>
+          <Col />
+        </Row>
+        {(isEnded == 'true') && <Row className="mt-5 text-center">
+          <Col/>
+          <Col>
+            <Alert className="border-warning border-5" variant="warning"><Alert.Heading >Session électorale terminée vous ne pouvez pas voter</Alert.Heading></Alert>
+          </Col>
+          <Col/>
+        </Row>}
+        {(isEnded == 'false') && <Row className="mt-5 text-center">
+          <Col/>
+          <Col><Alert variant="info" className="border-info border-5" ><Alert.Heading>Sélectionnez une adresse ensuite votez ou déléguez votre vote</Alert.Heading></Alert></Col>
+          <Col/>
+        </Row>}
+        <Row className="mt-3">
+          <Col className="float-start">
+            <Form.Select  style={{width: '60%',marginLeft: 12}}  onChange={onSelection.bind(this)} >
+              <option defaultValue>Selectionnez une adresse de vote</option>
+              {renderVoters().map((item) => {
+                // console.log(item);
+                return <option key={item} value={item} >{item}</option>;
+              })}
+            </Form.Select>
+          </Col>
+          <Col />
+        </Row>
+        <Row className="mt-5">
+          <Col>
+            <Stack style={{marginLeft: 12}} direction="horizontal" gap={3}>
+              {value && <div className=""><h5><Badge bg="success">Électeur</Badge></h5></div>}
+              {value && <div><h5><Badge style={{fontWeight: '12px'}} className="text-dark" bg="light"><b>{value}</b></Badge></h5></div>}
+              {/*<div>Third item</div>*/}
+            </Stack>
+          </Col>
+          <Col />
+        </Row>
+        <Row className="mt-5">
+          {renderCandidates()}
+        </Row>
       </Container>
     );
 
