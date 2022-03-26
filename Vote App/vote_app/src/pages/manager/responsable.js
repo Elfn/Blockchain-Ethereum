6bc235b23b72b43cb143a1ec67ebe83c6e23fbd5
Elimane Fofana <elimane.fofana@supinfo.com>
2022-03-27 00:05:15 +0100

    Functional components better than class components

--- Vote App/vote_app/src/pages/manager/responsable.js
+++ Vote App/vote_app/src/pages/manager/responsable.js
@@ -5,73 +5,66 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import {Alert, Badge, Button, Card, CardGroup, Col, Container, Form, Modal, Row, Spinner, Stack} from "react-bootstrap";
 import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
+import {useState, useEffect} from "react";
 
-class Responsable extends Component {
+function Responsable(props) {
 
-  constructor(props) {
-    super(props);
-    this.getCards = this.getCards.bind(this);
-    this.getCandidates = this.getCandidates.bind(this);
-    this.onSelectDate = this.onSelectDate.bind(this);
-    this.onAddCandidate = this.onAddCandidate.bind(this);
-    this.onShowListModal = this.onShowListModal.bind(this);
-    console.log('OOOOOOOOOOOOOOO');
+  const [accounts, setAccount] = useState('');
+  const [responsibleAddr, setResponsibleAddr] = useState('');
+  const [election, setElection] = useState(Election);
+  const [candidateCount, setCandidateCount] = useState('');
+  const [candidates, setCandidates] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [isEnd, setIsEnd] = useState('');
+  const [today, setToday] = useState('');
+  const [value, setValue] = useState('');
+  const [candidateName, setCandidateName] = useState('');
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showListModal, setShowListModal] = useState(false);
+  const [showDatePickerModal, setShowDatePickerModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isAdded, setIsAdded] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [winnerName, setWinnerName] = useState('');
 
+  useEffect(() => {
 
-    this.state = {
-      accounts: '',
-      responsibleAddr: '',
-      election: '',
-      candidateCount: '',
-      candidates: '',
-      endDate: '',
-      isEnd: '',
-      today: '',
-      value: '',
-      candidateName: '',
-      showAddModal: false,
-      showListModal: false,
-      showDatePickerModal: false,
-      loading: false,
-      isAdded: false,
-      errorMessage: '',
-      winnerName: ''
-    };
-
-    const election = Election;
     web3.eth.getAccounts().then(res => {
-      this.setState({accounts: res})
+      setAccount(res)
     });
     //this.setState({election: election});
 
 
-    election.methods.candidateCount().call().then(count => this.setState({
-      candidateCount: count
-    }));
-    this.onCheckDate();
-  }
+    election.methods.candidateCount().call().then(count => setCandidateCount(count));
+    //onCheckDate();
 
-  getDateFormat(date){
+  }, []);
+
+
+
+
+
+  const getDateFormat = (date) => {
     return ((date.getDate() < 10) ?  '0'+date.getDate() : date.getDate())+'/'+((date.getMonth() + 1) < 10 ? '0'+(date.getMonth() + 1) : (date.getMonth() + 1))+'/'+date.getFullYear();
   }
 
-   onShowListModal(){
-    this.setState({showListModal: true});
-    this.getCandidates().then(p => p);
+  const onShowListModal = () => {
+    setShowListModal(true);
+    getCandidates().then(p => p);
   }
 
-  getCards() {
+  const getCards = () => {
     const items = [
       [
         {
-          title: this.state.accounts.length - 1,
+          title: accounts.length - 1,
           subtitle: 'Nombre de comptes',
           text: 'Nombre de comptes disponibles pour les votes',
           buttons: []
           // style: {overflowWrap: 'break-word', marginLeft: 12, width: 18}
         },
         {
-          title: this.state.accounts[0],
+          title: accounts[0],
           subtitle: 'Manager adresse',
           text: 'Adresse du manager de l\'election',
           buttons: []
@@ -80,12 +73,12 @@ class Responsable extends Component {
       ],
       [
         {
-          title: this.state.candidateCount,
+          title: candidateCount,
           subtitle: 'Nombre de candidats',
           text: 'Nombre de candidats de la session électorale',
           buttons: [
-            <Button onClick={this.onShowListModal} variant="outline-primary" type="submit">Lister</Button>,
-            <Button style={{marginLeft: 12}} onClick={() => this.setState({showAddModal: true})} type="submit" variant="outline-success">Ajouter</Button>
+            <Button onClick={onShowListModal} variant="outline-primary" type="submit">Lister</Button>,
+            <Button style={{marginLeft: 12}} onClick={() => setShowAddModal(true)} type="submit" variant="outline-success">Ajouter</Button>
           ]
           // style: {overflowWrap: 'break-word', marginLeft: 12, width: 18}
         }
@@ -102,8 +95,8 @@ class Responsable extends Component {
           subtitle: '',
           text: 'Determiner la fin d l\'élection',
           buttons: [
-            <Button onClick={() => this.setState({showDatePickerModal: true})} type="submit" variant="outline-primary">Plannifier</Button>,
-            <h5>Date de fin: <span className="badge bg-secondary mt-3">{this.getDateFormat(new Date(localStorage.getItem('value')))}</span></h5>
+            <Button onClick={() => setShowDatePickerModal(true)} type="submit" variant="outline-primary">Plannifier</Button>,
+            <h5>Date de fin: <span className="badge bg-secondary mt-3">{getDateFormat(new Date(localStorage.getItem('value')))}</span></h5>
           ]
           // style: {overflowWrap: 'break-word', marginLeft: 12, width: 18}
         }
@@ -113,23 +106,22 @@ class Responsable extends Component {
     return Array.from(items);
   }
 
-  async getCandidates(){
+  const getCandidates = async () => {
     console.log('AAAAAAAAA');
-    const election = Election;
     const allCandidates = await Promise.all(
-      Array(parseInt(this.state.candidateCount))
+      Array(parseInt(candidateCount))
         .fill()
         .map((element, index) => {
           return election.methods.candidates(index).call();
         })
     );
-    this.setState({candidates: allCandidates});
-    console.log("AALLLAALLLAAALLLAALL => ",this.state.candidates);
+    setCandidates(allCandidates);
+    console.log("AALLLAALLLAAALLLAALL => ",candidates);
     //return allCandidates;
   }
 
-  renderCandidates(){
-    return Array.from(this.state.candidates).map((item, index)=>{
+  const renderCandidates = () => {
+    return Array.from(candidates).map((item, index)=>{
       let imgUrl = `https://bootdey.com/img/Content/avatar/avatar${item.id}.png`;
       return <Card key={index} style={{ width: '40px'}}>
           <Card.Img variant="top" src={imgUrl} />
@@ -141,87 +133,70 @@ class Responsable extends Component {
     });
   }
 
-   onSelectDate(e) {
+   const onSelectDate = (e) => {
      let endDate = new Date(e.target.value).toLocaleDateString("fr-FR");
-     const election = Election;
-     this.setState({value: e.target.value});
+     setValue(e.target.value);
      localStorage.setItem('value',e.target.value);
-     this.setState({endDate: endDate});
+     setEndDate(endDate);
      const todayDate = new Date().toLocaleDateString("fr-FR");
-     this.setState({today: todayDate});
+     setToday(todayDate);
      const  isEndOfElection = (endDate === todayDate);
      // console.log(new Date(e.target.value).toLocaleDateString("fr-FR"));
-     this.setState({isEnd: isEndOfElection});
+     setIsEnd(isEndOfElection);
      localStorage.setItem('isEnd', isEndOfElection);
      console.log('IN RESPONSIBLE ',isEndOfElection);
-     this.props.onGetElectionStatus(isEndOfElection);
+     props.onGetElectionStatus(isEndOfElection);
      if(localStorage.getItem('isEnd') == 'true'){
        election.methods.winner().call().then((name) => {
-            this.setState({winnerName: name})
+            setWinnerName(name);
             localStorage.setItem('winner',name);
          console.log(name);
        });
      }
   }
 
-  onCheckDate(){
-    let endDate = new Date(localStorage.getItem('value')).toLocaleDateString("fr-FR");
-    console.log(endDate);
-    const todayDate = new Date().toLocaleDateString("fr-FR");
-    if(todayDate === endDate){
-      this.props.onGetElectionStatus(true);
-      this.setState({isEnd: true});
-      localStorage.setItem('isEnd', true)
-    }else{
-      this.props.onGetElectionStatus(false);
-      this.setState({isEnd: false});
-      localStorage.setItem('isEnd', false);
-    }
-  }
-
-    onAddCandidate = async (event) => {
+  const onAddCandidate = async (event) => {
     //Prevent the "Submit" function to be executed unintentionally
     event.preventDefault();
-      if(this.state.candidateName == ''){
-        this.setState({errorMessage: 'Saisissez une valeur'});
-        this.setState({loading: false});
+      if(candidateName == ''){
+        setErrorMessage('Saisissez une valeur');
+        setLoading(false);
         return;
       }
-      this.setState({loading: true, errorMessage: ''});
-      const election = Election;
-      console.log(this.state.candidateName);
+      setLoading(true);
+      setErrorMessage('');
+      console.log(candidateName);
 
     try {
 
-        await election.methods.addCandidate(web3.utils.fromAscii(this.state.candidateName)).send({
-          from: this.state.accounts[0]
+        await election.methods.addCandidate(web3.utils.fromAscii(candidateName)).send({
+          from: accounts[0]
         });
 
       console.log('OK',event.target.value);
-      this.setState({isAdded: true});
+      setIsAdded(true);
       window.location.reload('/responsable');
 
     }catch (err) {
-      this.setState({errorMessage: err.message});
+      setErrorMessage(err.message);
     }
 
-    this.setState({loading: false});
-     this.setState({candidateName: ''});
+    setLoading(false);
+    setCandidateName('');
 
 
   }
 
 
 
-   handleClose = () => {
-    this.setState({showAddModal: false});
-    this.setState({showListModal: false});
-    this.setState({showDatePickerModal: false});
-    this.setState({isAdded: false});
+  const handleClose = () => {
+    setShowAddModal(false);
+    setShowListModal(false);
+    setShowDatePickerModal(false);
+    setIsAdded(false);
 
   };
 
-  render() {
     return (
 
       <Container>
@@ -233,7 +208,7 @@ class Responsable extends Component {
           <Col className="mt-4">
             <Stack direction="horizontal" gap={3}>
               <h5><Badge bg="warning">Responsable</Badge></h5>
-              <h5><Badge className="text-black" style={{fontWeight: '12px'}} bg="light"><b>{this.state.accounts[0]}</b></Badge></h5>
+              <h5><Badge className="text-black" style={{fontWeight: '12px'}} bg="light"><b>{accounts[0]}</b></Badge></h5>
               {(localStorage.getItem('isEnd') == 'true') && <Button variant="primary" style={{fontWeight: '12px'}}>
                 <Stack direction="horizontal" gap={3}>
                   <div>Vainqueur</div>
@@ -244,7 +219,7 @@ class Responsable extends Component {
           </Col>
           <Col />
         </Row>
-        {this.getCards().map((item,index) => {
+        {getCards().map((item,index) => {
           return <Row key={index} className="mt-4">
             {item.map((subitem,subindex) => {
               return <Col key={subindex} >
@@ -269,8 +244,8 @@ class Responsable extends Component {
         {/*Modal Lister*/}
         <Modal
           size="lg"
-          show={this.state.showListModal}
-          onHide={this.handleClose}
+          show={showListModal}
+          onHide={handleClose}
           aria-labelledby="example-modal-sizes-title-lg"
         >
           <Modal.Header closeButton>
@@ -281,12 +256,12 @@ class Responsable extends Component {
           <Modal.Body>
             <Container>
               <CardGroup>
-                {this.renderCandidates()}
+                {renderCandidates()}
               </CardGroup>
             </Container>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
+            <Button variant="secondary" onClick={handleClose}>
               Fermer
             </Button>
           </Modal.Footer>
@@ -294,8 +269,8 @@ class Responsable extends Component {
         {/*Modal Ajouter*/}
         <Modal
           size="md"
-          show={this.state.showAddModal}
-          onHide={this.handleClose}
+          show={showAddModal}
+          onHide={handleClose}
           aria-labelledby="example-modal-sizes-title-lg"
         >
           <Modal.Header closeButton>
@@ -306,18 +281,18 @@ class Responsable extends Component {
           <Modal.Body>
             {/*FORMULAIRE D'AJOUT*/}
             <Form className="text-center">
-              {(this.state.isAdded == true) && <Alert variant="success"><Alert.Heading>Candidate ajouté</Alert.Heading></Alert>}
-              {(this.state.errorMessage) && <Alert variant="danger"><Alert.Heading>{this.state.errorMessage}</Alert.Heading></Alert>}
+              {(isAdded == true) && <Alert variant="success"><Alert.Heading>Candidate ajouté</Alert.Heading></Alert>}
+              {(errorMessage) && <Alert variant="danger"><Alert.Heading>{errorMessage}</Alert.Heading></Alert>}
               <Form.Group className="mb-3" controlId="formBasicName">
                 {/*<Form.Label>Email address</Form.Label>*/}
-                <Form.Control value={this.state.candidateName} onChange={event => this.setState({ candidateName: event.target.value })} className="text-center" type="text" placeholder="Entrez le nom du candidat" />
+                <Form.Control value={candidateName} onChange={event => setCandidateName(event.target.value)} className="text-center" type="text" placeholder="Entrez le nom du candidat" />
               </Form.Group>
               <Button
-                disabled={this.state.loading == true}
-                onClick={this.onAddCandidate}
+                disabled={loading == true}
+                onClick={onAddCandidate}
                 variant="primary"
                 type="submit">
-                {(this.state.loading == true) && <Spinner
+                {(loading == true) && <Spinner
                   as="span"
                   animation="grow"
                   size="sm"
@@ -329,7 +304,7 @@ class Responsable extends Component {
             </Form>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
+            <Button variant="secondary" onClick={handleClose}>
               Fermer
             </Button>
           </Modal.Footer>
@@ -337,8 +312,8 @@ class Responsable extends Component {
         {/*Modal Plannifer*/}
         <Modal
           size="md"
-          show={this.state.showDatePickerModal}
-          onHide={this.handleClose}
+          show={showDatePickerModal}
+          onHide={handleClose}
           aria-labelledby="example-modal-sizes-title-lg"
         >
           <Modal.Header closeButton>
@@ -354,9 +329,9 @@ class Responsable extends Component {
                     id="date"
                     label="Date"
                     type="date"
-                    onChange={this.onSelectDate}
+                    onChange={onSelectDate}
                     value={localStorage.getItem('value')}
-                    defaultValue={this.state.value}
+                    defaultValue={value}
                     inputProps={{ min: new Date().toLocaleDateString('fr-CA') }}
                     InputLabelProps={{
                       shrink: true
@@ -367,7 +342,7 @@ class Responsable extends Component {
             </Container>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
+            <Button variant="secondary" onClick={handleClose}>
               Fermer
             </Button>
           </Modal.Footer>
@@ -376,8 +351,6 @@ class Responsable extends Component {
       </Container>
 
     )
-  }
-
 
 }
 
